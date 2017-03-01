ea0d24be7f9332eb4f3c2e20c4ba35b7bd1189c9
Dimitry Volker <dimitry.volker@gmail.com>
2017-03-01 19:23:24 +0100

    Extracted an method object from my own getExtraData function.

--- common/src/main/java/com/frostwire/bittorrent/BTDownload.java
+++ common/src/main/java/com/frostwire/bittorrent/BTDownload.java
@@ -644,46 +644,7 @@ public final class BTDownload implements BittorrentDownload {
 
     /* @CLEANCODE: Dimitry Volker */
     private Map<String, String> getExtraData(){
-        Map<String, String> map = new HashMap<>();
-        String infoHash = getInfoHash();
-        File file = engine.resumeDataFile(infoHash);
-
-        if (file.exists()) {
-            string_entry_map dictionary = readExtraData(file);
-            map = putStringEntryMapIntoMap(dictionary);
-        }
-
-        return map;
-    }
-
-    private Map<String, String> putStringEntryMapIntoMap(string_entry_map dictionary){
-        Map<String, String> map = new HashMap<>();
-        string_vector keys = dictionary.keys();
-        int size = (int) keys.size();
-
-        for (int i = 0; i < size; i++) {
-            String key = keys.get(i);
-            entry e = dictionary.get(k);
-            if (e.type() == entry.data_type.string_t) {
-                map.put(key, e.string());
-            }
-        }
-        return map;
-    }
-
-    /* @CLEANCODE: Dimitry Volker */
-    private string_entry_map readExtraData(File file){
-        try{
-            byte[] arr = FileUtils.readFileToByteArray(file);
-            entry e = entry.bdecode(Vectors.bytes2byte_vector(arr));
-            string_entry_map d = e.dict();
-            if (d.has_key(EXTRA_DATA_KEY)) {
-                return d.get(EXTRA_DATA_KEY).dict();
-            }
-        }catch (Exception e){
-            LOG.error("Error reading extra data from resume file", e);
-        }
-        return new string_entry_map();
+        return new ExtraData().invoke();
     }
 
     public boolean wasPaused() {
@@ -745,6 +706,51 @@ public final class BTDownload implements BittorrentDownload {
                     doResumeData(true);
                     break;
             }
+        }
+    }
+
+    private class ExtraData {
+        public Map<String, String> invoke() {
+            Map<String, String> map = new HashMap<>();
+            String infoHash = getInfoHash();
+            File file = engine.resumeDataFile(infoHash);
+
+            if (file.exists()) {
+                string_entry_map dictionary = readExtraData(file);
+                map = putStringEntryMapIntoMap(dictionary);
+            }
+
+            return map;
+        }
+
+        /* @CLEANCODE: Dimitry Volker */
+        private string_entry_map readExtraData(File file){
+            try{
+                byte[] arr = FileUtils.readFileToByteArray(file);
+                entry e = entry.bdecode(Vectors.bytes2byte_vector(arr));
+                string_entry_map d = e.dict();
+                if (d.has_key(EXTRA_DATA_KEY)) {
+                    return d.get(EXTRA_DATA_KEY).dict();
+                }
+            }catch (Exception e){
+                LOG.error("Error reading extra data from resume file", e);
+            }
+            return new string_entry_map();
+        }
+
+        private Map<String, String> putStringEntryMapIntoMap(string_entry_map dictionary){
+            Map<String, String> map = new HashMap<>();
+            string_vector keys = dictionary.keys();
+            int size = (int) keys.size();
+
+            for (int i = 0; i < size; i++) {
+                String key = keys.get(i);
+                entry e = dictionary.get(k);
+                if (e.type() == entry.data_type.string_t) {
+                    map.put(key, e.string());
+                }
+            }
+            return map;
         }
     }
 }
